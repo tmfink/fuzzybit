0b45649875477522ea9888e5efcf20de6118a48c
Travis Finkenauer <tmfink@umich.edu>
2015-10-09 16:05:15 -0400

    Increment version to v0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(name='fuzzybit',
-      version='0.1.0',
+      version='0.1.1',
       description='Fuzzy bit library',
       long_description=readme(),
       classifiers=[
